d4103f91baf5a84835d428e631d43e817306317b
Geir Ola Tvinnereim <gotvinne@stud.ntnu.no>
2020-02-18 16:09:57 +0100

    merge 2

--- skeleton_project/source/queue.c
+++ skeleton_project/source/queue.c
@@ -1,31 +1,48 @@
 #include "queue.h"
 #include "user.h"
 
+void buttom_queue_init(){
+	Order new_order;
+	make_order(-1,HARDWARE_ORDER_INSIDE,&new_order);
+
+for (unsigned int i = 0; i < BUTTOM_QUEUE_LENGTH; i++){
+	buttom_queue[i] = (Order*)malloc(sizeof(Order));
+	buttom_queue[i] = new_order; 
+}
+
+void queue_delete_order(unsigned int position){
+	if (position > 3){
+		printf("Error unvalid position\n");
+		return; 
+	}
+	order_queue[position].floor_destination = -1; 
+}
+
+void buttom_delete_order(){
+
+
+void queue_clear_all_orders(){
+	
+	for (unsigned int i = 0; i < ORDER_QUEUE_LENGTH; i++){
+		queue_delete_order(i);
+	}
+	
+	
+}
+
 void attatch_order_to_queue(Order* new_o){
 
     for (unsigned int i = 0; i < p_order_queue.size(); i++){
         
         if (p_order_queue[i] == -1){
             p_order_queue[i] = new_o->floor_destination; 
-            break; //usikker her
+            return; 
         }
     }
     else{
         printf("Order-queue is full");
     }
 
-}
-
-
-void queue_attach_order_to_button_queue(Order new_o){
-	p_button_queue[queue_size_of_button_queue()] = new_o;
-}
-
-//ikkje kommentert dette
-void queue_remove_order_from_button_queue(){
-	for(unsigned int i = 0; i < queue_size_of_button_queue(p_button_queue); i++){
-		p_button_queue[i] = p_button_queue[i+1];
-	}
 }
 
 
@@ -36,7 +53,7 @@ int queue_floor_arrived(){
         return 0;
 }
 
-//veit ikkje om denne er heilt nødvendig å ha...
+
 int queue_execute_new_order(){
     int s = 0;
     for(int i = 0, i < 4; i++){
@@ -48,21 +65,6 @@ int queue_execute_new_order(){
     return 1;
 }
 
-int queue_size_of_button_queue(b_q){
-	int size = 0;
-	while(b_q->floor_destination != -1){
-		size++;
-		b_q++;
-	}
-	return size;
-}
 
-
-void queue_initialize_empty_button_queue(){
-	for(int i = 0; i < queue_size_of_button_queue(q_button_queue); i++){
-		p_button_queue[i].floor_destination = -1;
-		p_button_queue[i].order_type = HARDWARE_ORDER_INSIDE;
-	}
-}
 
 
