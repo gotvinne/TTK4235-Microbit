d3084f56c20cca4686ff82041f2da7e74b3e6223
Geir Ola Tvinnereim <gotvinne@stud.ntnu.no>
2020-02-18 16:02:25 +0100

    merge

--- skeleton_project/source/user.h
+++ skeleton_project/source/user.h
@@ -1,12 +1,11 @@
+#include "hardware.h"
 /**
- * @file
- * @brief User impementation
+ * @brief user impementation
  * 
  */
 
-
 /**
- * @brief Order struct, holds @p order_floor and @p order_type
+ * @brief order struct, holds @p order_floor and @p order_type
  */ 
 typedef struct{
     int floor_destination; 
@@ -14,34 +13,20 @@ typedef struct{
 } Order; 
 
 /**
- * @brief Constructs Order struct
+ * @brief constructs Order struct
  * @param[in] order floor @p order_floor
  * @param[in] ordertype, up, down and inside @p order_type
  * @param[in] order pointer @p new. 
  */
-void new_order(int order_floor, HardwareOrder order_type, Order* new_o);
+Order make_order(int order_floor, HardwareOrder order_type);
 
 /**
- * @brief Add order struct into @p p_buttom_queue[]
- * @param[in] A const struct order @p new_order
+ * @brief add order struct into @p buttom_queue[]
+ * @param[in] an const struct order @p new_order
  * 
- * @warning Increments @p buttom_queue_length . 
- * @warning Allocates an order pointer
+ * @warning allocates a order pointer
  */
-void set_order(const Order new_o); 
-
-/**
- * @brief Getting user input from panel both inside and outside the elevator and generates a @c Order out of it
- *
- * @return Returns an @c Order with including floor_destination and HardwareOrder.
- */
-Order user_get_order();
-
-/**
- * @brief Attaches the @p order to the global p_button_queue.
- * @param order Is the order from the panel. Using iser_get_order() as this parameter.
- */
-void user_attach_order_to_button_queue();
+void set_order(const Order new_order);
 
 /**
  * @brief Events that can occure in the different states in @c e_elevator_state
@@ -50,13 +35,13 @@ typedef enum{
         stop_button_pressed,
         floor_arrived,
         obstruction_button_pressed,
-       	calibrating_finished,
-	execute_new_order
+	execute_new_order,
+        calibrating_finished,
 }e_elevator_event;
 
 
 /**
- * @brief Finding what signals the user are triggering.
+ * @brief Finding what signals the user are triggering
  * @warning It exists events that the user can not do himself, but this is calibrating conditions (don't care)
  * @return Returns the event that the user is affecting the system with
  */
